6d804cea4ec52e306bea88c7d4477617c547c618
mansipatel1012 <manmit1612@gmail.com>
2023-03-16 12:07:29 +0000

    My extra homework week6

---
+++ src/newhomeworkweek1/Programme1_BioData.java
@@ -0,0 +1,28 @@
+package newhomeworkweek1;
+
+/**
+ * Write a programme that prints your BioData.
+ */
+
+public class Programme1_BioData {
+
+    public static void main(String[] args) {
+        System.out.println("           BIO DATA             ");
+        System.out.println("Name                : Mansi Patel");
+        System.out.println("MobileNo            : 07658790970");
+        System.out.println("Email               : janemane@gmail.com");
+        System.out.println("Gender              : Female");
+        System.out.println("Date Of Birth       : 01.05.2000");
+        System.out.println("Marital Status      : single");
+        System.out.println("Language Known      : English,Spanish and German");
+        System.out.println("Qualilification     : Software Tester");
+        System.out.println("Experience          : Agile Tester with 5 years of experience");
+        System.out.println("Address             : 123 Abc Street, London AB1 2CD");
+        System.out.println("Place               : London");
+        System.out.println("Date                : 02.10.2021");
+        System.out.println("Signature           : MPatel");
+
+    }
+
+
+}
---
+++ src/newhomeworkweek1/Programme2_PrintFace.java
@@ -0,0 +1,15 @@
+package newhomeworkweek1;
+
+public class Programme2_PrintFace {
+    /**
+     * Write a Java programme to print a face.
+     */
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| O O |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '_' |");
+        System.out.println(" +_____+");
+    }
+
+}
